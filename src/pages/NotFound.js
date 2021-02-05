7f517ca9b6226e5e2ae056ef470b2993c0303ae6
Achour Benakli <73812092+AchourBenakli@users.noreply.github.com>
2021-02-06 00:46:42 +0100

    page5

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -6,7 +6,7 @@ const NotFound = () => {
     <div className="notFound">
       <div className="notFoundContent">
         <h3>Sorry, this page does not exist.</h3>
-        <h5>Please comeback to my Home Page</h5><br/><br/><br/>
+        <h5>Please comeback to my Home Page5</h5><br /><br /><br />
         <NavLink exact to="/">
           <i className="fas fa-home">
             <span>Home Page</span>
