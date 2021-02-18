a6015deaa470576fa924d89e54ccd6e7a49ea81b
Achour Benakli <73812092+AchourBenakli@users.noreply.github.com>
2021-02-18 23:20:54 +0100

    test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
       <div className="homeContent">
         <div className="content">
           <h1>Welcome</h1>
-          <h2>I'm a Front Desk Agent</h2>
+          <h2>I'm a Front Desk Agent.</h2>
           <a href="./CV.pdf" target="_blank">Download CV</a>
           <Modal />
         </div>
