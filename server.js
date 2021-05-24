f466f524c576cd32320a70f1010709ffb11638b5
Achour Benakli <73812092+AchourBenakli@users.noreply.github.com>
2021-05-24 02:52:22 +0200

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const sendMail = import(sendMail.js);
-const send = import(send.js);
+const sendDates = import(sendDates.js);
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
@@ -12,10 +12,12 @@ app.use(cors());
 app.get('/', (req, res) => {
   res.send('GET request to the homepage');
 })
-app.post('/api/senddates', send(req, res));
+
+app.post('/api/senddates', sendDates(req, res));
+
 app.post('/api/forma', sendEmail(req, res));
 
-`~~`
+
 app.listen(PORT, () => {
   console.log(`server starting at port ${PORT}`)
 })
