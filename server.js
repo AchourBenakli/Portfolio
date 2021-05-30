e6b525958faf4128aaff89b3c993d96d6a850182
Achour Benakli <73812092+AchourBenakli@users.noreply.github.com>
2021-05-30 22:55:15 +0200

    update

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const { sendEmail, contact } = require('./sendMail');
 const PORT = process.env.PORT || 3001;
+
+
+const buildPath = path.join(__dirname, '..', 'build');
+app.use(express.static(buildPath));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +16,8 @@ app.use(cors());
 app.get('/', (req, res) => {
   res.send('GET request to the homepage');
 })
+
+
 
 app.post('/api/senddates', contact);
 
